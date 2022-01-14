7acc1b6fedea0e51c0e7a076ab40e5bdf28c2dc6
Seungku Kim <seungkukim@kaist.ac.kr>
2022-01-14 17:26:44 +0900

    footer and header done

---
+++ src/components/Button.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Button.css';
+
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
+
+const COLOR = ['primary', 'blue', 'red', 'green'];
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize,
+    buttonColor
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+        ? buttonStyle
+        : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize)
+        ? buttonSize 
+        : SIZES[0];
+
+    const checkButtonColor = COLOR.includes(buttonColor)
+        ? buttonColor 
+        : null;
+
+    return (
+        <Link to='/sign-up' className='btn-mobile'>
+            <button
+                className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
+                onClick={onClick}
+                type={type}
+            >
+                {children}
+            </button>
+        </Link>
+    );
+};
